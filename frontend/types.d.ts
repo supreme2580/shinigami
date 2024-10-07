26d172981ed87ae4335b6b137b6e3cbde45de32c
supreme2580 <victoromorogbe69@gmail.com>
2024-10-07 21:00:13 +0100

    code cleanup

--- frontend/types.d.ts
+++ frontend/types.d.ts
@@ -5,3 +5,36 @@ export interface StackItem {
 export interface StackVisualizerProps {
     stackContent: StackItem[];
 }
+
+export interface EditorContentProps {
+    split: boolean;
+    scriptSig: string;
+    scriptPubKey: string;
+    setScriptSig: React.Dispatch<React.SetStateAction<string>>;
+    setScriptPubKey: React.Dispatch<React.SetStateAction<string>>;
+    setEditorTheme: (monaco: any, setMonaco: React.Dispatch<React.SetStateAction<any>>) => void;
+    setMonacoOne: React.Dispatch<React.SetStateAction<any>>;
+    setMonacoTwo: React.Dispatch<React.SetStateAction<any>>;
+    font: any;
+  }
+  
+  export interface EditorHeaderProps {
+    split: boolean;
+    onSplitToggle: () => void;
+  }
+  
+  export interface EditorControlsProps {
+    isFetching: boolean;
+    isDebugging: boolean;
+    isDebugFetch: boolean;
+    runError: string | null;
+    debugError: string | null;
+    hasFetchedDebugData: boolean;
+    step: number;
+    debuggingContent: StackItem[][];
+    onRunScript: () => void;
+    onDebugScript: () => void;
+    onPreviousStep: () => void;
+    onNextStep: () => void;
+    onStopDebugging: () => void;
+  }
